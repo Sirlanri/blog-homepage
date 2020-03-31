298f7b93b2c10970aa8636fbe05b562e8c64863f
深蓝PC <mail@ri-co.cn>
2020-03-31 14:20:31 +0800

    顶栏不用背景图

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,60 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    
+    <v-card class="overflow-hidden">
+      <v-app-bar
+        absolute
+        color="#000000"
+        dark
+        shrink-on-scroll
+        fade-img-on-scroll
+        src="@/assets/PC/white-grey.jpg"
+        scroll-target="#scrolling-techniques-2"
+      >
+        <template v-slot:img="{ props }">
+          <v-img v-bind="props" gradient="to top, rgba(0, 0, 0, 0.05), rgba(0, 0, 0, 0.33)"></v-img>
+        </template>
+
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+        <v-toolbar-title>深蓝的小站</v-toolbar-title>
+
+        <v-spacer></v-spacer>
+
+        <v-btn icon>
+          <v-icon>mdi-magnify</v-icon>
+        </v-btn>
+
+        <v-btn icon>
+          <v-icon>mdi-heart</v-icon>
+        </v-btn>
+
+        <v-btn icon>
+          <v-icon>mdi-dots-vertical</v-icon>
+        </v-btn>
+      </v-app-bar>
+      <v-sheet id="scrolling-techniques-2" class="overflow-y-auto" max-height="1000">
+        <v-container></v-container>
+      </v-sheet>
+    </v-card>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  components: {}
+};
 </script>
+
+<style lang="less">
+.home{
+  height: 1000px;
+  background: url(http://127.0.0.1:8887/pc/white-grey.jpg);
+  background-size: 100% 100%;
+  background-repeat: no-repeat;
+}
+.test{
+  color: rgba(255, 255, 255, 0.329),rgba(226, 239, 250, 0.445);
+}
+</style>
