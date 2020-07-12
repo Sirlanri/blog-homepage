a1dc15c22b51ee4904761f8372aed6333204550d
深蓝PC <mail@ri-co.cn>
2020-07-12 16:23:13 +0800

    为卡片添加鼠标hover注释

--- src/components/FriendCard.vue
+++ src/components/FriendCard.vue
@@ -1,6 +1,6 @@
 <template>
   <v-card class="mx-auto" max-width="500" >
-      <v-img height="200px" class="align-end img1"
+      <v-img height="200px" class="align-end img1" @click="visit"
       :src="picAddress" gradient="to bottom , rgba(0,0,0,.0), rgba(0,0,0,.5)">
         <v-card-text class="cardtext">
             {{introduction}} 
@@ -13,13 +13,19 @@
       <v-card-actions v-if="isroot">
           <v-btn text small>修改</v-btn>
           <v-btn text small color="error">删除</v-btn>
-          <v-btn outlined icon>
+          <v-btn outlined large class="jumpbtn" @click="visit">
               <v-icon>mdi-forward</v-icon>
           </v-btn>
       </v-card-actions>
 
       <v-card-actions v-else>
-          <v-icon v-if="ssl" class="ssl">mdi-shield-lock-outline</v-icon>
+          <v-tooltip bottom v-if="ssl">
+              <template v-slot:activator="{ on, attrs }">
+                <v-icon class="ssl" v-bind="attrs" v-on="on">mdi-shield-lock-outline</v-icon>
+              </template>
+              <span>已开启SSL加密</span>
+          </v-tooltip>
+          
           <v-icon v-else class="ssl">mdi-lock-open-alert</v-icon>
           <v-icon v-if="speed<=100" color="green">mdi-speedometer</v-icon>
           <v-icon v-if="speed<=1000&&speed>100" color="#998a09">mdi-speedometer-medium</v-icon>
@@ -27,7 +33,7 @@
           <span class="msnum" v-if="speed<999">{{speed}}ms</span>
           <span class="msnum" v-else>999+ms</span>
           
-          <v-btn outlined  large class="jumpbtn">
+          <v-btn outlined  large class="jumpbtn" @click="visit">
               <v-icon>mdi-forward</v-icon>
           </v-btn>
       </v-card-actions>
@@ -55,6 +61,9 @@ export default {
     methods:{
         edit(){
             store.commit('changeId',this.id)
+        },
+        visit(){
+            window.open(this.siteAddress)
         }
     }
 }
--- src/views/Friends.vue
+++ src/views/Friends.vue
@@ -48,6 +48,9 @@ export default {
 
       ]
     }
+  },
+  methods:{
+    
   }
 }
 </script>
